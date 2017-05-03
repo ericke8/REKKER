57eeb57d675ffce3afc89ca02accaca23d444c85
ericke8 <keric.yi@gmail.com>
2017-05-03 08:18:41 -0700

    selectwindow

--- Menu.java
+++
@@ -1,4 +0,0 @@
-public class Menu
-{
-    private Queue<Character> units = new PriorityQueue<Character>();
-}
---
+++ SelectWindow.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class SelectWindow
+{
+    private Set<Character> units = new TreeSet<Character>();
+}
