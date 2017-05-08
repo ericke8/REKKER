9c9e3a4bcccfb005e1c33f54210bbe0f85f17fb9
ericke8 <keric.yi@gmail.com>
2017-05-08 08:15:57 -0700

    field

--- Field.java
+++ Field.java
@@ -1,116 +1,135 @@
-package rekker;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class Field 
+
+public class Field
 {
-	private final JPanel gui = new JPanel(new BorderLayout(3, 3));
-    private JButton[][] fieldBoxes = new JButton[8][8];
-    private JPanel field;
-    private final JLabel message = new JLabel(
-            "Chess Champ is ready to play!");
+
+    private final JPanel gui = new JPanel( new BorderLayout( 3, 3 ) );
+
+    private JButton[][] chessBoardSquares = new JButton[8][8];
+
+    private JPanel chessBoard;
+
+    private final JLabel message = new JLabel( "REKKER is ready to play!" );
+
     private static final String COLS = "ABCDEFGH";
 
-    public Field() 
+
+    Field()
     {
         initializeGui();
     }
 
-    public final void initializeGui() {
+
+    public final void initializeGui()
+    {
         // set up the main GUI
-        gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+        gui.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
         JToolBar tools = new JToolBar();
-        tools.setFloatable(false);
-        gui.add(tools, BorderLayout.PAGE_START);
-        tools.add(new JButton("New")); // TODO - add functionality!
-        tools.add(new JButton("Save")); // TODO - add functionality!
-        tools.add(new JButton("Restore")); // TODO - add functionality!
+        tools.setFloatable( false );
+        gui.add( tools, BorderLayout.PAGE_START );
+        tools.add( new JButton( "New Game" ) ); // TODO - add functionality!
+        tools.add( new JButton( "Move" ) ); // TODO - add functionality!
+        tools.add( new JButton( "Attack" ) ); // TODO - add functionality!
         tools.addSeparator();
-        tools.add(new JButton("Resign")); // TODO - add functionality!
+        tools.add( new JButton( "End Turn" ) ); // TODO - add functionality!
         tools.addSeparator();
-        tools.add(message);
+        tools.add( message );
 
-        gui.add(new JLabel("?"), BorderLayout.LINE_START);
+        gui.add( new JLabel( "?" ), BorderLayout.LINE_START );
 
-        field = new JPanel(new GridLayout(0, 9));
-        field.setBorder(new LineBorder(Color.BLACK));
-        gui.add(field);
+        chessBoard = new JPanel( new GridLayout( 0, 9 ) );
+        chessBoard.setBorder( new LineBorder( Color.BLACK ) );
+        gui.add( chessBoard );
 
-        // create the fields squares
-        Insets buttonMargin = new Insets(0,0,0,0);
-        for (int i = 0; i < fieldBoxes.length; i++) {
-            for (int j = 0; j < fieldBoxes[i].length; j++) {
+        // create the chess board squares
+        Insets buttonMargin = new Insets( 0, 0, 0, 0 );
+        for ( int ii = 0; ii < chessBoardSquares.length; ii++ )
+        {
+            for ( int jj = 0; jj < chessBoardSquares[ii].length; jj++ )
+            {
                 JButton b = new JButton();
-                b.setMargin(buttonMargin);
+                b.setMargin( buttonMargin );
                 // our chess pieces are 64x64 px in size, so we'll
                 // 'fill this in' using a transparent icon..
-                ImageIcon icon = new ImageIcon(
-                        new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
-                b.setIcon(icon);
-                if ((j % 2 == 1 && i % 2 == 1)
-                        //) {
-                        || (j % 2 == 0 && i % 2 == 0)) {
-                    b.setBackground(Color.WHITE);
-                } else {
-                    b.setBackground(Color.BLACK);
+                ImageIcon icon = new ImageIcon( new BufferedImage( 64, 64, BufferedImage.TYPE_INT_ARGB ) );
+                b.setIcon( icon );
+                if ( ( jj % 2 == 1 && ii % 2 == 1 )
+                    // ) {
+                    || ( jj % 2 == 0 && ii % 2 == 0 ) )
+                {
+                    b.setBackground( Color.WHITE );
                 }
-                fieldBoxes[j][i] = b;
+                else
+                {
+                    b.setBackground( Color.WHITE );
+                }
+                chessBoardSquares[jj][ii] = b;
             }
         }
 
-        //fill the chess board
-        field.add(new JLabel(""));
+        // fill the chess board
+        chessBoard.add( new JLabel( "" ) );
         // fill the top row
-        for (int i = 0; i < 8; i++) {
-            field.add(
-                    new JLabel(COLS.substring(i, i + 1),
-                    SwingConstants.CENTER));
+        for ( int ii = 0; ii < 8; ii++ )
+        {
+            chessBoard.add( new JLabel( COLS.substring( ii, ii + 1 ), SwingConstants.CENTER ) );
         }
         // fill the black non-pawn piece row
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                switch (j) {
+        for ( int ii = 0; ii < 8; ii++ )
+        {
+            for ( int jj = 0; jj < 8; jj++ )
+            {
+                switch ( jj )
+                {
                     case 0:
-                        field.add(new JLabel("" + (i + 1),
-                                SwingConstants.CENTER));
+                        chessBoard.add( new JLabel( "" + ( ii + 1 ), SwingConstants.CENTER ) );
                     default:
-                        field.add(fieldBoxes[j][i]);
+                        chessBoard.add( chessBoardSquares[jj][ii] );
                 }
             }
         }
     }
 
-    public final JComponent getChessBoard() {
-        return field;
+
+    public final JComponent getChessBoard()
+    {
+        return chessBoard;
     }
 
-    public final JComponent getGui() {
+
+    public final JComponent getGui()
+    {
         return gui;
     }
 
-    public static void main(String[] args) {
-        Runnable r = new Runnable() {
+
+    public static void main( String[] args )
+    {
+        Runnable r = new Runnable()
+        {
 
             @Override
-            public void run() {
+            public void run()
+            {
                 Field cb = new Field();
 
-                JFrame f = new JFrame("ChessChamp");
-                f.add(cb.getGui());
-                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                f.setLocationByPlatform(true);
+                JFrame f = new JFrame( "ChessChamp" );
+                f.add( cb.getGui() );
+                f.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+                f.setLocationByPlatform( true );
 
                 // ensures the frame is the minimum size it needs to be
                 // in order display the components within it
                 f.pack();
                 // ensures the minimum size is enforced.
-                f.setMinimumSize(f.getSize());
-                f.setVisible(true);
+                f.setMinimumSize( f.getSize() );
+                f.setVisible( true );
             }
         };
-        SwingUtilities.invokeLater(r);
+        SwingUtilities.invokeLater( r );
     }
 }
